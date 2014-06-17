a10f63ffe67eccc0219530e15b926d0264f16547
ilkerhalil@gmail.com <ilkerhalil@gmail.com>
2014-06-17 11:44:19 +0300

    bug changes

--- PaymentPlanClient/ViewModels/CalculateRefreshPaymentPlanResponse.cs
+++ PaymentPlanClient/ViewModels/CalculateRefreshPaymentPlanResponse.cs
@@ -1,17 +1,20 @@
 ﻿namespace PaymentPlanClient.ViewModels
 {
     using System.Collections.Generic;
+    using System.Runtime.Serialization;
 
     using PaymentPlanClient.Entity;
 
     /// <summary>
     /// 
     /// </summary>
+    [DataContract]
     public class CalculateRefreshPaymentPlanResponse
     {
         /// <summary>
         /// 
         /// </summary>
+        [DataMember]
         public IEnumerable<RefreshPaymentPlan> RefreshPaymentPlans { get; set; }
     }
 }
