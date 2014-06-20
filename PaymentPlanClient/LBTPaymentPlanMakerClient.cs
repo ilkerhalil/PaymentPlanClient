e745b046623cad5c6dc0b14577162b05c6ce80f5
ilkerhalil@gmail.com <ilkerhalil@gmail.com>
2014-06-20 10:42:52 +0300

    bug changes

--- PaymentPlanClient/LBTPaymentPlanMakerClient.cs
+++ PaymentPlanClient/LBTPaymentPlanMakerClient.cs
@@ -185,7 +185,7 @@
         #region PhoneManipulation
         public AddAdressResponse AddPhone(AddPhoneRequest addPhoneRequest)
         {
-            var restRequest = new RestRequest(Resources.LBTEmail_Url, Method.DELETE) { RequestFormat = DataFormat.Json };
+            var restRequest = new RestRequest(Resources.LBTPhone_Url, Method.DELETE) { RequestFormat = DataFormat.Json };
             restRequest.AddBody(addPhoneRequest);
             var response = restClient.Execute<AddAdressResponse>(restRequest);
             IRestResponse = response;
@@ -196,7 +196,7 @@
 
         public DeletePhoneResponse DeletePhone(DeletePhoneRequest deletePhoneRequest)
         {
-            var restRequest = new RestRequest(Resources.LBTEmail_Url, Method.DELETE) { RequestFormat = DataFormat.Json };
+            var restRequest = new RestRequest(Resources.LBTPhone_Url, Method.DELETE) { RequestFormat = DataFormat.Json };
             restRequest.AddBody(deletePhoneRequest);
             var response = restClient.Execute<DeletePhoneResponse>(restRequest);
             IRestResponse = response;
