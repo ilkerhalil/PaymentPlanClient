605530e3f35f91942b987e5e23a44b1979f6195b
ilkerhalil@gmail.com <ilkerhalil@gmail.com>
2014-07-31 18:24:20 +0300

    add complimant help

--- PaymentPlanClient/ViewModels/ComplaintManipulation/AddComplaintRequest.cs
+++ PaymentPlanClient/ViewModels/ComplaintManipulation/AddComplaintRequest.cs
@@ -11,39 +11,40 @@
 
 
         /// <summary>
-        /// kontak id
+        /// varsa kontak id'si,giriş yapmadan girilmişse boş geçilebilir
+        /// 
         /// </summary>
         [DataMember]
-        public int ContactId { get; set; }
+        public int? ContactId { get; set; }
 
         /// <summary>
-        /// 
+        /// formdan gelen e-posta bilgisi
         /// </summary>
         [DataMember]
         public string Email { get; set; }
 
 
         /// <summary>
-        /// 
+        /// Sikayet edenin adi soyadi
         /// </summary>
         [DataMember]
         public string EntityName { get; set; }
 
         /// <summary>
-        /// 
+        /// formdan gelen telefon numarası
         /// </summary>
         ///
         [DataMember]
         public string PhoneNo { get; set; }
 
         /// <summary>
-        /// 
+        /// Sikayet tipi yoksa bos gecilebilir.
         /// </summary>
         [DataMember]
-        public int ComplaintTopicId { get; set; }
+        public int? ComplaintTopicId { get; set; }
 
         /// <summary>
-        /// şikayet içeriği
+        /// formdan gelen sikayet icerigi
         /// </summary>
         [DataMember]
         public string Explanation { get; set; }
